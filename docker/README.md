777fd5212ee3fca2061bb049d52550912039d534
Harsh <hrsht.rastogi13@gmail.com>
2020-02-26 00:09:23 -0800

    first commit

---
+++ docker/README.md
@@ -0,0 +1,43 @@
+## Workshop Material: Introduction to Machine Learning with Apache Spark and Redis
+
+
+## Runtime Requirments
+1. Docker (at least 2 CPU cores and 8gb RAM)
+2. System Terminal (iTerm, Terminal, etc)
+3. Working Web Browser (Chrome or Firefox)
+
+### Technologies Used
+1. [Apache Zeppelin](https://zeppelin.apache.org/docs/latest/interpreter/spark.html)
+2. [Apache Spark](http://spark.apache.org/)
+3. [Mysql]
+
+#### Spark 2.4.5
+- http://mirror.metrocast.net/apache/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz
+- (222 MB)
+
+
+### Docker
+Install Docker Desktop (https://www.docker.com/products/docker-desktop)
+
+Additional Docker Resources:
+* https://docs.docker.com/get-started/
+* https://hub.docker.com/
+
+#### Docker Runtime Recommendations
+1. 2 or more cpu cores.
+2. 8gb/ram or higher.
+
+## Installation
+1. Install Docker (See Docker above)
+2. Once Docker is installed. Open up your terminal application and `cd /spark-intro/docker`
+3. `./run.sh install`
+4. `./run.sh start`
+
+## Checking Zeppelin and Updating Zeppelin
+1. The **Main Application** should now be running at http://localhost:8080/
+
+
+### Update the Zeppelin Spark Interpreter Runtime
+1. Go to http://localhost:8080/#/interpreter on your Web Browser
+2. Search for `spark` in the `Search Interpreters` input field.
+3. Click the `edit` button to initiate editing mode.
---
+++ docker/run.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+PWD=${PWD}
+DATA_DIR=${PWD}/data/
+SPARK_VERSION='2.4.5'
+DOCKER_COMPOSE_FILE='docker-compose-all.yaml'
+DOCKER_NETWORK_NAME='learnsql'
+
+function installSpark() {
+    echo "using curl to download spark 2.4.5"
+    #mkdir ${PWD}/install
+
+    if test ! -d "${PWD}/spark-${SPARK_VERSION}"
+    then 
+      curl -XGET http://mirror.metrocast.net/apache/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoop2.7.tgz > ${PWD}/install/spark-2.4.5.tgz
+      cd ${PWD}/install && tar -xvzf spark-2.4.5.tgz && rm spark-${SPARK_VERSION}.tgz
+      mv spark-${SPARK_VERSION}-bin-hadoop2.7 ../spark-${SPARK_VERSION}
+      cd ..
+    else
+      echo "Spark is already installed under ${PWD}/spark-${SPARK_VERSION}"
+    fi
+    echo "${PWD}"
+}
+
+
+function sparkConf() {
+    cp ${PWD}/install/spark-defaults.conf ${PWD}/spark-${SPARK_VERSION}/conf/
+}
+
+function init() {
+   installSpark
+   sparkConf
+}
+
+function cleanAndBuildDockerNetwork() {
+    echo "cleaning up docker network if it was already created. Network Name : ${DOCKER_NETWORK_NAME}"
+    docker network rm ${DOCKER_NETWORK_NAME}
+    echo "building new docker network : ${DOCKER_NETWORK_NAME}"
+    docker network create -d bridge ${DOCKER_NETWORK_NAME}
+}
+
+function cleanDocker() {
+    docker rm -f `docker ps -aq` # deletes the old containers
+}
+
+function start() {
+    #init
+    export SPARK_HOME=${PWD}/"spark-${SPARK_VERSION}"
+    echo "Your Spark Home is set to ${SPARK_HOME}"
+    cleanDocker
+    cleanAndBuildDockerNetwork
+    # docker-compose -f ${DOCKER_COMPOSE_FILE} up -d --remove-orphans redis5
+    docker-compose -f ${DOCKER_COMPOSE_FILE} up -d --remove-orphans zeppelin
+}
+
+function stop() {
+    docker-compose -f ${DOCKER_COMPOSE_FILE} down
+}
+
+function info() {
+    CONTAINER=$2
+    echo "INSPECT THIS ${CONTAINER}"
+    docker inspect ${CONTAINER}
+}
+
+case "$1" in
+    install)
+        init
+    ;;
+    prep)
+        prepData
+    ;;
+    start)
+        start
+    ;;
+    stop)
+        stop
+    ;;
+    info)
+        info
+    ;;
+    *)
+        echo $"Usage: $0 {install | prep | start | stop | info {CONTAINER_NAME}"
+    ;;
+esac
